d57b9163d0a9942477c108763b6d94ee01242734
陈书剑 <61688818+yukikanae@users.noreply.github.com>
2020-07-06 19:42:44 +0800

    更新

--- javascript/call.js
+++ javascript/call.js
@@ -1,7 +1,45 @@
-function checkinput() {
+/* eslint-disable */	
+
+function checkin() {
+	var x=document.getElementById("log1").value;
+    var y=document.getElementById("log2").value;
+	var z=document.getElementById("log3").value;//读取表单数据
+	var patrn=/^\w\w{5,10}$/;;
 	var index=document.getElementById("select").selectedIndex;
-    var val=document.getElementById("select").options[index].value; 
- 
+    var val=document.getElementById("select").options[index].value; //读取选择数据
+   if (x == '') {
+        alert("用户名不能为空");
+        return false;
+    }
+
+    if (y == '') {
+        alert("密码不能为空");
+        return false;
+    }
+	
+	if (z == '') {
+        alert("建议内容不能为空");
+        return false;
+    }
+	//表单均不能为空
+	
+	if(patrn.test(x)) 
+	   {
+        alert("用户名提交成功");
+    }
+    else {
+        alert("用户名提交失败,请输入5至10位字母或数字");
+    }
+	
+	if(patrn.test(y)) 
+	   {
+        alert("密码提交成功");
+    }
+    else {
+        alert("密码提交失败,请输入5至10位字母或数字");
+    }
+	//验证用户名和密码是否符合规则
+	
 	switch(val) 
     {
      case "男":
@@ -11,6 +49,7 @@ function checkinput() {
            alert("女士，感谢您的提交");
            break;
      default:
-             alert("出现错误请返回");
-     }
+             alert("发生错误，请属性界面");
+     }//选择判断
 }
+
